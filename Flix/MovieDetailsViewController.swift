96a616e22f0ee058d7bf87eee21c48a694b3a5ff
Chris Koehne <cdkoehne@gmail.com>
2021-02-07 13:03:10 -0500

    Added detail view, collection tab, and collection detail navigation

---
+++ Flix/MovieDetailsViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MovieDetailsViewController.swift
+//  Flix
+//
+//  Created by Chris Koehne on 2/1/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    
+    var movie: [String:Any]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        print(movie!)
+
+        // Do any additional setup after loading the view.
+        titleLabel.text = movie["title"] as? String
+//        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        let numVotes = movie["vote_count"] as! Int
+        let rating = movie["vote_average"] as! Double
+        ratingLabel.text = String(format: "%.1f / 10 \n(\(numVotes))", rating)
+        ratingLabel.sizeToFit()
+        
+        let basePosterUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: basePosterUrl + posterPath)
+        posterView.af.setImage(withURL: posterUrl!)
+        
+        let baseBackdropUrl = "https://image.tmdb.org/t/p/w780"
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: baseBackdropUrl + backdropPath)
+        backdropView.af.setImage(withURL: backdropUrl!)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Flix/MovieGridViewController.swift
@@ -0,0 +1,87 @@
+//
+//  MovieGridViewController.swift
+//  Flix
+//
+//  Created by Chris Koehne on 2/6/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var movies = [[String:Any]]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        let cellsPerLine: CGFloat = 2
+        let interItemSpacingTotal = layout.minimumInteritemSpacing * (cellsPerLine - 1)
+        let width = collectionView.frame.size.width / cellsPerLine - interItemSpacingTotal / cellsPerLine
+        layout.itemSize = CGSize(width: width, height: width / 0.69)
+
+
+        let url = URL(string: "https://api.themoviedb.org/3/movie/299537/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+              // TODO: Get the array of movies
+            self.movies = dataDictionary["results"] as! [[String: Any]]
+            self.collectionView.reloadData()
+//            print(self.movies)
+              // TODO: Store the movies in a property to use elsewhere
+              // TODO: Reload your table view data
+
+           }
+        }
+        task.resume()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+        
+        let movie = movies[indexPath.item]
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w780"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        cell.posterView.af.setImage(withURL: posterUrl!)
+        
+        return cell
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+    print("hello")
+     let cell = sender as! UICollectionViewCell
+     let indexPath = collectionView.indexPath(for: cell)!
+     let movie = movies[indexPath.item]
+     let detailsViewController = segue.destination as! MovieDetailsViewController
+     detailsViewController.movie = movie
+     collectionView.deselectItem(at: indexPath, animated: true)
+ }
+
+}
